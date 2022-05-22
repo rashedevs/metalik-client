00e85cf511dfdbaa50f821375287c5d70d3dc174
rashedevs <rasheduap2015@gmail.com>
2022-05-22 16:54:40 +0600

    navbar added and router implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import Navbar from "./Pages/Shared/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Blogs from "./Pages/Blogs/Blogs";
+import MyPortfolio from "./Pages/MyPortfolio/MyPortfolio";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <button class="btn btn-primary">Button</button>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="blogs" element={<Blogs></Blogs>} />
+        <Route path="portfolio" element={<MyPortfolio></MyPortfolio>} />
+        <Route path="login" element={<Login></Login>} />
+      </Routes>
     </div>
   );
 }
