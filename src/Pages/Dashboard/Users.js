9c67717ebb6ed9223153223b11668b22b9d7eaf3
rashedevs <rasheduap2015@gmail.com>
2022-05-24 18:35:36 +0600

    using react query to load all users on users page

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const Users = () => {
+  const { data, isLoading } = useQuery("users", () =>
+    fetch("http://localhost:5000/user").then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h2 className="text-2xl">All users</h2>
+      <h2 className="text-xl text-purple-600 text-center">All users </h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>User</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user, index) => (
+              <tr key={index} order={user}>
+                <th>{index + 1}</th>
+                <td>{user.email}</td>
+
+                <td>
+                  <button className="btn btn-xs  ">Make Admin</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
