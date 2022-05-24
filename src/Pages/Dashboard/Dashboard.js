51a284eb433869beb400ca2f2945fff15726ee88
rashedevs <rasheduap2015@gmail.com>
2022-05-24 21:52:49 +0600

    securing routes for admin and applying conditional rendering

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../firebase.init";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const [admin] = useAdmin(user);
   return (
     <div className="drawer drawer-mobile">
       <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
@@ -19,24 +24,36 @@ const Dashboard = () => {
           <li>
             <Link to="/dashboard">My Profile</Link>
           </li>
-          <li>
-            <Link to="/dashboard/orders">My Orders</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/review">Add A Review</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/manageorders">Manage All Orders</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/addproduct">Add A Product</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/makeadmin">Make Admin</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/manageproducts">Manage Products</Link>
-          </li>
+          {!admin && (
+            <li>
+              <Link to="/dashboard/orders">My Orders</Link>
+            </li>
+          )}
+          {!admin && (
+            <li>
+              <Link to="/dashboard/review">Add A Review</Link>
+            </li>
+          )}
+          {admin && (
+            <li>
+              <Link to="/dashboard/manageorders">Manage All Orders</Link>
+            </li>
+          )}
+          {admin && (
+            <li>
+              <Link to="/dashboard/addproduct">Add A Product</Link>
+            </li>
+          )}
+          {admin && (
+            <li>
+              <Link to="/dashboard/makeadmin">Make Admin</Link>
+            </li>
+          )}
+          {admin && (
+            <li>
+              <Link to="/dashboard/manageproducts">Manage Products</Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
